b500b4bc28235e6d827925c7637625fec5fa333c
Marcosp3 <marcosp2837@gmail.com>
2024-09-30 22:11:33 -0300

    cria funcao para calculo de soma e subtracao

---
+++ calculo.js
@@ -0,0 +1,24 @@
+//Cálculo usando função:
+
+function soma(x, y) {
+    return a + b;
+}
+
+function subtrai(x, y) {
+    return a - b;
+}
+
+console.log("Resultado soma: " + soma(10, 5))
+
+console.log("Resultado subtração: " + subtrai(10, 5))
+
+//Opção 2:
+
+// function somaSubtrai(x, y) {
+//     let soma = n1 + n2;
+//     let subtracao = n1 - n2;
+//     console.log("Soma: ", soma);
+//     console.log("Subtração: ", subtracao);
+// }
+
+// somaSubtrai(10, 5)
