9a0f79880ffdaf663ff95e2fc75a4ff52b459799
Marcosp3 <marcosp2837@gmail.com>
2024-10-02 20:52:47 -0300

    Atividade para por em pratica os comandos do github

--- media.js
+++ media.js
@@ -1,6 +1,10 @@
 //Atividade para testar os comandos do GitHub.
 
-let media = 5
+let nota1 = 5
+let nota2 = 7
+let nota3 = 8
+
+let media = (nota1 + nota2 + nota3) / 3
 
 if(media >= 6) {
     console.log("Aprovado")
