d804f63ef0a2f88a57925313bb3deeaf1390c625
Marcosp3 <marcosp2837@gmail.com>
2024-10-02 21:13:41 -0300

    array de frutas modificado

--- frutas.js
+++ frutas.js
@@ -1,5 +1,10 @@
+//Array criado para utilizar os comandos do github
+
 const frutas = ["maca", "manga", "morango", "melancia", "maracuja", "acerola"];
 
 console.log("A fruta na posição 1 é: ", frutas[1]);
 console.log("A fruta na posição 4 é: ", frutas[4]);
 console.log("Meu array tem", frutas.length, "frutas");
+
+frutas.push("abacate")
+console.log(frutas)
